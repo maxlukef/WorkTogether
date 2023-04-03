d8170b6b24137d7d68aae2413c54156aeb5035c6
Alex Childs <achilds97@outlook.com>
2023-04-03 14:18:19 -0600

    added some password stuff

--- WorkTogether/Models/WT_DBContext.cs
+++ WorkTogether/Models/WT_DBContext.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.EntityFrameworkCore;
+using MySql.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace WorkTogether.Models
 {
@@ -13,9 +17,13 @@ namespace WorkTogether.Models
         protected readonly IConfiguration Configuration;
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
+            if (!options.IsConfigured)
+            {
+                //options.UseMySQL("server=localhost;port=3306;user=root;password=;database=database");
+            }
             // connect to mysql with connection string from app settings
             var connectionString = Configuration.GetConnectionString("WebApiDatabase");
-            options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+            //options.UseMySQL("Server = localhost; Port = 3306; Database = worktogether; User ID = test; Password = test;");
         }
 
         public DbSet<TodoItem> TodoItems { get; set; } = null!;
--- WorkTogether/Program.cs
+++ WorkTogether/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Identity.Web;
+using WorkTogether.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +11,8 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<WorkTogether.Models.WT_DBContext>();
+builder.Services.AddDbContext<WT_DBContext>(options => options.UseMySQL(builder.Configuration.GetConnectionString("DefaultConnection")));
+//builder.Services.AddDbContext<WorkTogether.Models.WT_DBContext>(options => { builder.Configuration.GetConnectionString("DefaultConnection");});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
