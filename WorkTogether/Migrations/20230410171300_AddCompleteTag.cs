c140fe9f33da4d1283da8116612a44762392079d
maxlukef <u1253508@utah.edu>
2023-04-10 11:13:25 -0600

    Added a migration for the complete tag. Ref #64

---
+++ WorkTogether/Migrations/20230410171300_AddCompleteTag.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WorkTogether.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCompleteTag : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Complete",
+                table: "Teams",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Complete",
+                table: "Teams");
+        }
+    }
+}
