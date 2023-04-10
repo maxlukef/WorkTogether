4a9270e6578efd6d1a43979ff5cf91f13690fea0
maxlukef <u1253508@utah.edu>
2023-04-10 11:11:18 -0600

    Added a tag to the team class to represent whether the team is complete or incomplete (basically what phase they're in) Ref #64

--- WorkTogether/Models/Team.cs
+++ WorkTogether/Models/Team.cs
@@ -11,6 +11,11 @@ namespace WorkTogether.Models
         public string Name { get; set; }
         public ICollection<User> Members { get; set; }
 
+        /// <summary>
+        /// Whether or not a team is complete - i.e. is it still in the team building phase (False), or has it moved onto the project stage (True)
+        /// </summary>
+        public bool Complete { get; set; }
+
         Project? Project { get; set; }
     }
 }
