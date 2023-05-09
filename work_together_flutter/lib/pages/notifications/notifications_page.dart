70c0ae9b2825f5fd2f6c642f3687ab8d3c1a18a5
Grayson Spencer <graysonspencer1212@gmail.com>
2023-05-08 22:32:43 -0600

    Created a basic version of the notification page.

--- work_together_flutter/lib/pages/notifications/notifications_page.dart
+++ work_together_flutter/lib/pages/notifications/notifications_page.dart
@@ -14,10 +14,80 @@ class NotificationsPage extends StatefulWidget {
 class _NotificationsPageState extends State<NotificationsPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color(0xFFFFFFFF),
-      appBar: CustomAppBar(title: "Notifications"),
-      body: Center(child: Text("Notifications Coming Soon")),
+    List<Widget> notifications = [];
+
+    // Hardcoded notifcations.
+    notifications.add(
+        createNotification("CS 4400", "Complete Weekly Survey", "03/24/2023"));
+    notifications.add(
+        createNotification("CS 5530", "Complete Peer Review", "07/23/1994"));
+    notifications.add(
+        createNotification("CS 4400", "Complete Weekly Survey", "03/24/2023"));
+    notifications.add(
+        createNotification("CS 5530", "Complete Peer Review", "07/23/1994"));
+    notifications.add(
+        createNotification("CS 4400", "Complete Weekly Survey", "03/24/2023"));
+    notifications.add(
+        createNotification("CS 5530", "Complete Peer Review", "07/23/1994"));
+    notifications.add(
+        createNotification("CS 4400", "Complete Weekly Survey", "03/24/2023"));
+    notifications.add(
+        createNotification("CS 5530", "Complete Peer Review", "07/23/1994"));
+    notifications.add(
+        createNotification("CS 4400", "Complete Weekly Survey", "03/24/2023"));
+    notifications.add(
+        createNotification("CS 5530", "Complete Peer Review", "07/23/1994"));
+
+    return Scaffold(
+        backgroundColor: const Color(0xFFFFFFFF),
+        appBar: const CustomAppBar(title: "Notifications"),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListView.separated(
+              padding: const EdgeInsets.all(8),
+              itemCount: notifications.length,
+              separatorBuilder: (BuildContext context, int index) =>
+                  const Divider(
+                    color: Colors.black,
+                    thickness: 2.0,
+                  ),
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  child: notifications[index],
+                );
+              }),
+        ));
+  }
+
+  Widget createNotification(String className, String title, String dueDate) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          className,
+          style: const TextStyle(fontWeight: FontWeight.normal, fontSize: 15),
+        ),
+        Text(
+          title,
+          style: const TextStyle(fontWeight: FontWeight.normal, fontSize: 23),
+        ),
+        Row(
+          children: [
+            const Text(
+              "Due: ",
+              style: TextStyle(fontWeight: FontWeight.normal, fontSize: 15),
+            ),
+            Text(
+              dueDate,
+              style: const TextStyle(
+                  fontWeight: FontWeight.normal,
+                  fontSize: 15,
+                  color: Color.fromARGB(255, 31, 152, 252)),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
