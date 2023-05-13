120d77df82250b215d6ca5a5c4b9b6d570982c8b
Grayson Spencer <graysonspencer1212@gmail.com>
2023-05-13 12:13:31 -0600

    Basic Page Layouts and Navigations.

--- work_together_flutter/lib/pages/chat/chat_page.dart
+++ work_together_flutter/lib/pages/chat/chat_page.dart
@@ -12,12 +12,58 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
+  List<Widget> contacts = [];
+  double dividerGap = 20.0;
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color(0xFFFFFFFF),
-      appBar: CustomAppBar(title: "Chat"),
-      body: Center(child: Text("Chat Coming Soon")),
+    contacts.add(createContact("Gary"));
+
+    return Scaffold(
+      backgroundColor: const Color(0xFFFFFFFF),
+      appBar: const CustomAppBar(
+        title: "Chat",
+      ),
+      body: ListView.builder(
+          padding: const EdgeInsets.all(8),
+          itemCount: contacts.length,
+          itemBuilder: (BuildContext context, int index) {
+            return ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(
+                    builder: (context) {
+                      return const ChatPage();
+                    },
+                  ));
+                },
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: contacts[index],
+                ));
+          }),
+    );
+  }
+
+  // Eventually add a profile picture to the contacts.
+  Widget createContact(String name) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        // Replace this with profile picture.
+        const Padding(
+          padding: EdgeInsets.fromLTRB(8.0, 0, 24, 0),
+          child: Icon(
+            Icons.account_circle,
+            color: Colors.blue,
+            size: 50.0,
+          ),
+        ),
+        Text(
+          name,
+          style: const TextStyle(fontWeight: FontWeight.normal, fontSize: 18),
+        ),
+      ],
     );
   }
 }
