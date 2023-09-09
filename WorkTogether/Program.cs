054f79e403dc1a51081d666071c4f5397c7658d8
maxlukef <u1253508@utah.edu>
2023-09-09 12:38:28 -0600

    Got rid of password reqs

--- WorkTogether/Program.cs
+++ WorkTogether/Program.cs
@@ -85,6 +85,17 @@ builder.Services.AddAuthentication(options =>
         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWTAuth:SecretKey"]))
     };
 });
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    // Default Password settings.
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 1;
+    options.Password.RequiredUniqueChars = 1;
+});
 builder.Services.AddControllers();
 
 
