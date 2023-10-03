e555fec96d4bf3e3e0d95a226f5b3a03890c3d05
maxlukef <u1253508@utah.edu>
2023-10-03 11:58:25 -0600

    FInished getters for tasks. All task endpoints are implemented. Still need to test them before making a merge request but feel free to play around with them.

--- WorkTogether/Controllers/TasksController.cs
+++ WorkTogether/Controllers/TasksController.cs
@@ -33,6 +33,11 @@ namespace WorkTogether.Controllers
         }
 
         // GET: api/Tasks/5
+        /// <summary>
+        /// Gets a task by its id. The student getting the task must be a member of the team.
+        /// </summary>
+        /// <param name="id">The id of the task</param>
+        /// <returns>a TaskDTO</returns>
         [HttpGet("{id}")]
         [Authorize]
         public async Task<ActionResult<BasicTaskDTO>> GetTask(int id)
@@ -59,9 +64,9 @@ namespace WorkTogether.Controllers
             return taskDTO;
         }
 
-        // GET: api/UserTasks
+        // GET: api/Tasks/UserTasks
         /// <summary>
-        /// Gets all tasks for the user signed in.
+        /// Gets all incomplete tasks for the user signed in.
         /// </summary>
         /// <returns>List of tasks for the current user</returns>
         [Authorize]
@@ -83,12 +88,362 @@ namespace WorkTogether.Controllers
             return new ObjectResult(tasks_dto);
         }
 
+        // GET: api/Tasks/AllUserTasks
+        /// <summary>
+        /// Gets all tasks(complete or not) for the user signed in.
+        /// </summary>
+        /// <returns>List of task dtos for the current user</returns>
+        [Authorize]
+        [HttpGet("AllUserTasks")]
+        public async Task<ActionResult<BasicTaskDTO>> GetAllUserTasks()
+        {
+            User curr_user = await _um.GetUserAsync(User);
+            if (_context.Tasks == null)
+            {
+                return NotFound();
+            }
+            var tasks = await _context.Tasks.Include(t => t.Assignees).Where(t => t.Assignees.Contains(curr_user)).ToListAsync();
+
+            List<BasicTaskDTO> tasks_dto = new List<BasicTaskDTO>();
+            foreach (TaskItem task in tasks)
+            {
+                tasks_dto.Add(TaskToBasicDTO(task));
+            }
+
+            return new ObjectResult(tasks_dto);
+        }
 
 
+        // GET: api/GroupTasks/2
+        /// <summary>
+        /// Gets all incomplete tasks for the group
+        /// </summary>
+        /// <param name="teamid">The id of the team</param>
+        /// <returns>List of task dtos</returns>
+        [Authorize]
+        [HttpGet("GroupTasks/{teamid}")]
+        public async Task<ActionResult<BasicTaskDTO>> GetGroupTasks(int teamid)
+        {
+            User curr_user = await _um.GetUserAsync(User);
+            if (_context.Tasks == null)
+            {
+                return NotFound();
+            }
+
+            var team = await _context.Teams.Where(te=> te.Id == teamid).Include(te=>te.Members).FirstOrDefaultAsync();
+
+            if (team == null)
+            {
+                return NotFound();
+            }
+
+            if(!team.Members.Contains(curr_user))
+            {
+                return Unauthorized();
+            }
+            var tasks = await _context.Tasks.Include(t => t.Team).Include(t=>t.Assignees).Where(t=> t.Team.Id == teamid && !t.Completed).ToListAsync();
 
 
+            List<BasicTaskDTO> tasks_dto = new List<BasicTaskDTO>();
+            foreach (TaskItem task in tasks)
+            {
+                tasks_dto.Add(TaskToBasicDTO(task));
+            }
 
-        //USE THIS ONE
+            return new ObjectResult(tasks_dto);
+        }
+
+
+        // GET: api/Tasks/AllGroupTasks/2
+        /// <summary>
+        /// Gets all tasks for the group(complete or not)
+        /// </summary>
+        /// <param name="teamid">The id of the team to get tasks for</param>
+        /// <returns>List of task dtos</returns>
+        [Authorize]
+        [HttpGet("AllGroupTasks/{teamid}")]
+        public async Task<ActionResult<BasicTaskDTO>> GetAllGroupTasks(int teamid)
+        {
+            User curr_user = await _um.GetUserAsync(User);
+            if (_context.Tasks == null)
+            {
+                return NotFound();
+            }
+
+            var team = await _context.Teams.Where(te => te.Id == teamid).Include(te => te.Members).FirstOrDefaultAsync();
+
+            if (team == null)
+            {
+                return NotFound();
+            }
+
+            if (!team.Members.Contains(curr_user))
+            {
+                return Unauthorized();
+            }
+            var tasks = await _context.Tasks.Include(t => t.Team).Include(t => t.Assignees).Where(t => t.Team.Id == teamid).ToListAsync();
+
+
+            List<BasicTaskDTO> tasks_dto = new List<BasicTaskDTO>();
+            foreach (TaskItem task in tasks)
+            {
+                tasks_dto.Add(TaskToBasicDTO(task));
+            }
+
+            return new ObjectResult(tasks_dto);
+        }
+
+
+        // GET: api/Tasks/UserGroupTasks/2
+        /// <summary>
+        /// Gets all of the current users incomplete tasks for a specified group
+        /// </summary>
+        /// <param name="teamid">The id of the group to find tasks for</param>
+        /// <returns>List of task DTOs</returns>
+        [Authorize]
+        [HttpGet("UserGroupTasks/{teamid}")]
+        public async Task<ActionResult<BasicTaskDTO>> GetUserGroupTasks(int teamid)
+        {
+            User curr_user = await _um.GetUserAsync(User);
+            if (_context.Tasks == null)
+            {
+                return NotFound();
+            }
+
+            var team = await _context.Teams.Where(te => te.Id == teamid).Include(te => te.Members).FirstOrDefaultAsync();
+
+            if (team == null)
+            {
+                return NotFound();
+            }
+
+            if (!team.Members.Contains(curr_user))
+            {
+                return Unauthorized();
+            }
+            var tasks = await _context.Tasks.Include(t => t.Team).Include(t=>t.Assignees).Where(t => t.Team.Id == teamid && !t.Completed && t.Assignees.Contains(curr_user)).ToListAsync();
+
+
+            List<BasicTaskDTO> tasks_dto = new List<BasicTaskDTO>();
+            foreach (TaskItem task in tasks)
+            {
+                tasks_dto.Add(TaskToBasicDTO(task));
+            }
+
+            return new ObjectResult(tasks_dto);
+        }
+
+        //GET: api/Tasks/AllUserGroupTasks/2
+        /// <summary>
+        /// Gets all of the current users incomplete and complete tasks for a specified group
+        /// </summary>
+        /// <param name="teamid">The id of the group to find tasks for</param>
+        /// <returns>List of task DTOs</returns>
+        [Authorize]
+        [HttpGet("AllUserGroupTasks/{teamid}")]
+        public async Task<ActionResult<BasicTaskDTO>> GetAllUserGroupTasks(int teamid)
+        {
+            User curr_user = await _um.GetUserAsync(User);
+            if (_context.Tasks == null)
+            {
+                return NotFound();
+            }
+
+            var team = await _context.Teams.Where(te => te.Id == teamid).Include(te => te.Members).FirstOrDefaultAsync();
+
+            if (team == null)
+            {
+                return NotFound();
+            }
+
+            if (!team.Members.Contains(curr_user))
+            {
+                return Unauthorized();
+            }
+            var tasks = await _context.Tasks.Include(t => t.Team).Include(t => t.Assignees).Where(t => t.Team.Id == teamid && t.Assignees.Contains(curr_user)).ToListAsync();
+
+
+            List<BasicTaskDTO> tasks_dto = new List<BasicTaskDTO>();
+            foreach (TaskItem task in tasks)
+            {
+                tasks_dto.Add(TaskToBasicDTO(task));
+            }
+
+            return new ObjectResult(tasks_dto);
+        }
+
+
+        // GET: api/Tasks/MilestoneTasks/3
+        /// <summary>
+        /// Gets all incomplete tasks for the specified milestone
+        /// </summary>
+        /// <param name="msid">The id of the milestone</param>
+        /// <returns>List of tasks for the milestone</returns>
+        [Authorize]
+        [HttpGet("MilestoneTasks/{msid}")]
+        public async Task<ActionResult<BasicTaskDTO>> GetMilestoneTasks(int msid)
+        {
+            User curr_user = await _um.GetUserAsync(User);
+            if (_context.Tasks == null)
+            {
+                return NotFound();
+            }
+            var ms = await _context.Milestones.Include(m => m.tasks).Include(m => m.Project).FirstOrDefaultAsync();
+            if(ms == null) { return NotFound(); }
+            var team = await _context.Teams.Where(te => te.Project == ms.Project).Include(te=> te.Members).FirstOrDefaultAsync();
+
+
+            if (team == null)
+            {
+                return NotFound();
+            }
+
+            if (!team.Members.Contains(curr_user))
+            {
+                return Unauthorized();
+            }
+            var tasks = await _context.Tasks.Where(t => t.ParentMilestone == ms && !t.Completed).ToListAsync();
+
+
+            List<BasicTaskDTO> tasks_dto = new List<BasicTaskDTO>();
+            foreach (TaskItem task in tasks)
+            {
+                tasks_dto.Add(TaskToBasicDTO(task));
+            }
+
+            return new ObjectResult(tasks_dto);
+        }
+
+        // GET: api/Tasks/AllMilestoneTasks/3
+        /// <summary>
+        /// Gets all incomplete and complete tasks for the specified milestone
+        /// </summary>
+        /// <param name="msid">The id of the milestone</param>
+        /// <returns>List of tasks for the milestone</returns>
+        [Authorize]
+        [HttpGet("AllMilestoneTasks/{msid}")]
+        public async Task<ActionResult<BasicTaskDTO>> GetAllMilestoneTasks(int msid)
+        {
+            User curr_user = await _um.GetUserAsync(User);
+            if (_context.Tasks == null)
+            {
+                return NotFound();
+            }
+            var ms = await _context.Milestones.Include(m => m.tasks).Include(m => m.Project).FirstOrDefaultAsync();
+            if (ms == null) { return NotFound(); }
+            var team = await _context.Teams.Where(te => te.Project == ms.Project).Include(te => te.Members).FirstOrDefaultAsync();
+
+
+            if (team == null)
+            {
+                return NotFound();
+            }
+
+            if (!team.Members.Contains(curr_user))
+            {
+                return Unauthorized();
+            }
+            var tasks = await _context.Tasks.Where(t => t.ParentMilestone == ms).ToListAsync();
+
+
+            List<BasicTaskDTO> tasks_dto = new List<BasicTaskDTO>();
+            foreach (TaskItem task in tasks)
+            {
+                tasks_dto.Add(TaskToBasicDTO(task));
+            }
+
+            return new ObjectResult(tasks_dto);
+        }
+
+        // GET: api/Tasks/UserMilestoneTasks/3
+        /// <summary>
+        /// Gets all incomplete tasks for the specified milestone and current user
+        /// </summary>
+        /// <param name="msid">The id of the milestone</param>
+        /// <returns>List of tasks for the milestone</returns>
+        [Authorize]
+        [HttpGet("UserMilestoneTasks/{msid}")]
+        public async Task<ActionResult<BasicTaskDTO>> GetUserMilestoneTasks(int msid)
+        {
+            User curr_user = await _um.GetUserAsync(User);
+            if (_context.Tasks == null)
+            {
+                return NotFound();
+            }
+            var ms = await _context.Milestones.Include(m => m.tasks).Include(m => m.Project).FirstOrDefaultAsync();
+            if (ms == null) { return NotFound(); }
+            var team = await _context.Teams.Where(te => te.Project == ms.Project).Include(te => te.Members).FirstOrDefaultAsync();
+
+
+            if (team == null)
+            {
+                return NotFound();
+            }
+
+            if (!team.Members.Contains(curr_user))
+            {
+                return Unauthorized();
+            }
+            var tasks = await _context.Tasks.Include(t=>t.Assignees).Where(t => t.ParentMilestone == ms && !t.Completed && t.Assignees.Contains(curr_user)).ToListAsync();
+
+
+            List<BasicTaskDTO> tasks_dto = new List<BasicTaskDTO>();
+            foreach (TaskItem task in tasks)
+            {
+                tasks_dto.Add(TaskToBasicDTO(task));
+            }
+
+            return new ObjectResult(tasks_dto);
+        }
+
+
+        // GET: api/Tasks/AllUserMilestoneTasks/3
+        /// <summary>
+        /// Gets all incomplete and complete tasks for the specified milestone and current user
+        /// </summary>
+        /// <param name="msid">The id of the milestone</param>
+        /// <returns>List of tasks for the milestone</returns>
+        [Authorize]
+        [HttpGet("AllUserMilestoneTasks/{msid}")]
+        public async Task<ActionResult<BasicTaskDTO>> GetAllUserMilestoneTasks(int msid)
+        {
+            User curr_user = await _um.GetUserAsync(User);
+            if (_context.Tasks == null)
+            {
+                return NotFound();
+            }
+            var ms = await _context.Milestones.Include(m => m.tasks).Include(m => m.Project).FirstOrDefaultAsync();
+            if (ms == null) { return NotFound(); }
+            var team = await _context.Teams.Where(te => te.Project == ms.Project).Include(te => te.Members).FirstOrDefaultAsync();
+
+
+            if (team == null)
+            {
+                return NotFound();
+            }
+
+            if (!team.Members.Contains(curr_user))
+            {
+                return Unauthorized();
+            }
+            var tasks = await _context.Tasks.Include(t => t.Assignees).Where(t => t.ParentMilestone == ms && t.Assignees.Contains(curr_user)).ToListAsync();
+
+
+            List<BasicTaskDTO> tasks_dto = new List<BasicTaskDTO>();
+            foreach (TaskItem task in tasks)
+            {
+                tasks_dto.Add(TaskToBasicDTO(task));
+            }
+
+            return new ObjectResult(tasks_dto);
+        }
+
+        //POST: api/Tasks/create
+        /// <summary>
+        /// Creates a new task from a task DTO
+        /// </summary>
+        /// <param name="taskDTO">the DTO to create the new task from</param>
+        /// <returns>Ok if successful, error otherwise. problem, Notfound, or Unauthorized</returns>
         [HttpPost("create")]
         [Authorize]
         public async Task<ActionResult<BasicTaskDTO>> CreateTask(BasicTaskDTO taskDTO)
@@ -138,7 +493,12 @@ namespace WorkTogether.Controllers
             return Ok();
         }
 
-        //USE THIS ONE
+        //POST: api/Tasks/edit
+        /// <summary>
+        /// Edits a task using a TaskDTO
+        /// </summary>
+        /// <param name="taskDTO"></param>
+        /// <returns>Ok if successful</returns>
         [HttpPost("edit")]
         [Authorize]
         public async Task<ActionResult<BasicTaskDTO>> EditTask(BasicTaskDTO taskDTO)
@@ -187,8 +547,13 @@ namespace WorkTogether.Controllers
             return Ok();
         }
 
-
-        //USE THIS ONE
+        //POST: api/Tasks/assign/5/6
+        /// <summary>
+        /// Assigns a user to a task
+        /// </summary>
+        /// <param name="taskid">The id of the task to assign to</param>
+        /// <param name="userid">The id of the user to assign</param>
+        /// <returns>Ok if successful</returns>
         [HttpPost("assign/{taskid}/{userid}")]
         [Authorize]
         public async Task<ActionResult<BasicTaskDTO>> Assign(int taskid, int userid)
@@ -218,7 +583,13 @@ namespace WorkTogether.Controllers
             return Ok();
         }
 
-        //USE THIS ONE
+        //POST: api/tasks/unassign/5/6
+        /// <summary>
+        /// Unassign a user from a task
+        /// </summary>
+        /// <param name="taskid">The id of the task</param>
+        /// <param name="userid">The id of the user</param>
+        /// <returns>Ok if successful</returns>
         [HttpPost("unassign/{taskid}/{userid}")]
         [Authorize]
         public async Task<ActionResult<BasicTaskDTO>> Unassign(int taskid, int userid)
@@ -239,7 +610,7 @@ namespace WorkTogether.Controllers
             {
                 return NotFound();
             }
-            if (!team.Members.Contains(toassign) || !team.Members.Contains(curr) || !task.Assignees.Contains(curr))
+            if (!team.Members.Contains(toassign) || !team.Members.Contains(curr))
             {
                 return Unauthorized();
             }
@@ -248,7 +619,12 @@ namespace WorkTogether.Controllers
             return Ok();
         }
 
-        
+        //POST: api/tasks/markcomplete/id
+        /// <summary>
+        /// Mark a task as complete
+        /// </summary>
+        /// <param name="id">The id of the task</param>
+        /// <returns>Ok if successful</returns>
         [HttpPost("markcomplete/{id}")]
         [Authorize]
         public async Task<ActionResult<BasicTaskDTO>> MarkComplete(int id)
@@ -277,8 +653,47 @@ namespace WorkTogether.Controllers
            
         }
 
-            // DELETE: api/Tasks/5
-            [HttpDelete("{id}")]
+        //POST: api/tasks/markincomplete/id
+        /// <summary>
+        /// Marks a task incomplete
+        /// </summary>
+        /// <param name="id">THe task to mark incomplete</param>
+        /// <returns>Ok if successful</returns>
+        [HttpPost("markincomplete/{id}")]
+        [Authorize]
+        public async Task<ActionResult<BasicTaskDTO>> MarkIncomplete(int id)
+        {
+            User curr = GetCurrentUser(HttpContext);
+            if (_context.Tasks == null)
+            {
+                return NotFound();
+            }
+            var task = await _context.Tasks.Where(t => t.Id == id).Include(t => t.Team).FirstOrDefaultAsync();
+
+
+            if (task == null)
+            {
+                return NotFound();
+            }
+            var team = await _context.Teams.Where(te => te.Id == task.Team.Id).Include(te => te.Members).FirstOrDefaultAsync();
+            if (!team.Members.Contains(curr))
+            {
+                return Unauthorized("Not a member of the team for this task");
+            }
+            task.Completed = false;
+            _context.SaveChanges();
+            return Ok();
+
+
+        }
+
+        // DELETE: api/Tasks/5
+        /// <summary>
+        /// Deletes a task
+        /// </summary>
+        /// <param name="id">The id of the task</param>
+        /// <returns>Ok if successful</returns>
+        [HttpDelete("{id}")]
         [Authorize]
         public async Task<IActionResult> DeleteTask(int id)
         {
